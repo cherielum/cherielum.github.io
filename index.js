a37c8f6971f11f923753cca47bab0f293405c6ff
Cherie Lum <charismatic.cherie@gmail.com>
2018-06-01 15:59:38 -0400

    adding styles

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".icons").hover(function(){
+        $(this).css("background-color", "#02b3e4");
+        }, function(){
+        $(this).css("background-color", "white");
+    });
+    $(".projects").hover(function(){
+        $(this).css("background-color", "#02b3e4");
+        }, function(){
+        $(this).css("background-color", "#454242");
+    });
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+things to do:
+3. fix caption for each card
+4. add a readme doc
